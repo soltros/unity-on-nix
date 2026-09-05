c3d99bec65f49ea1b767e0e8607de5c4c4f800c5
Derrik Diener <soltros@proton.me>
2026-09-05 04:53:56 -0400

    network patches

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,7 @@ let
   base = import ./components.nix { inherit pkgs; };
   packages = base // (import ./desktop-components.nix { inherit pkgs base; }) // {
     cinnamon-session-unity = import ./cinnamon-session-unity.nix { inherit pkgs; };
+    lomiri-indicator-network = pkgs.lomiri.lomiri-indicator-network;
     compiz = pkgs.callPackage ./compiz.nix {};
     nux = pkgs.callPackage ./nux.nix {};
     unity = pkgs.callPackage ./unity.nix {
